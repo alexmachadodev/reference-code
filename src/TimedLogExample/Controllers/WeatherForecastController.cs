0b7f6d19c826427431656a2a9a34b18972f0c10f
Alex Machado <alex.machado@invillia.com>
2022-02-02 16:21:23 -0300

    New project timed log

---
+++ src/TimedLogExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using TimedLogExample.Logging;
+
+namespace TimedLogExample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> GetWeatherForecast()
+    {
+        using var _ = _logger.TimedOperation(nameof(GetWeatherForecast));
+        
+        Thread.Sleep(new Random().Next(1000, 8000));
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
---
+++ src/TimedLogExample/Logging/LoggerExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace TimedLogExample.Logging;
+
+public static class LoggerExtensions
+{
+    public static IDisposable TimedOperation<T>(this ILogger<T> logger, string message, params object?[] args)
+        => new TimedLogOperation<T>(logger, LogLevel.Information, message, args);
+}
